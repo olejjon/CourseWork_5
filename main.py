4809f601d910ba504dbe814132ae2aabd27b5047
olejjon123 <olejjon_123@mail.ru>
2023-05-30 19:36:35 +0300

    update complete

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ hh_api = HeadHunterAPI()
 superjob_api = SuperJobAPI()
 json_saver = JSONSaver()
 
+
 # Функция для взаимодействия с пользователем
 def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words):
+    """Запись и сортировка вакансий"""
     json_saver = JSONSaver()
     json_saver.add_vacancy_hh(hh_vacancies)
     json_saver.add_vacancy_superjob(superjob_vacancies)
@@ -21,11 +23,12 @@ def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words):
         writer._save()
         print('Файл успешно сохранен')
     except Exception:
-        print('Что-то пошло не так...')
+        print('Что-то пошло не так с сохранением Excel файла...')
 
 
 def user_interaction():
-    platforms = ["HeadHunter", "SuperJob"]
+    """Функция приема параметров и получения вакансий"""
+    # platforms = ["HeadHunter", "SuperJob"]
     search_query = input("Введите поисковый запрос: ")
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").lower().split()
